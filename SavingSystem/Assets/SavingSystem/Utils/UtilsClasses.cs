334a8248bc2114a4951ce20d83f17d8ccac629ae
Diovan Marques <diovanmarques@gmail.com>
2020-03-24 01:39:10 -0300

    - Added documentation - Added UtilsClasses

---
+++ SavingSystem/Assets/SavingSystem/Utils/UtilsClasses.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class ListData<T>
+{
+    public List<T> list;
+
+    public ListData() { }
+
+    public ListData(List<T> list)
+    {
+        this.list = list;
+    }
+}
